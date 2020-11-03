60416495c0592e8df5e53d6fd7a406ed89e7e577
winseywu <70607960+winseywu@users.noreply.github.com>
2020-11-02 21:26:34 -0500

    decide on another song

--- sketch.js
+++ sketch.js
@@ -1,10 +1,8 @@
-let KB;
-let KBA;
+let kardRM;
+let enemyAudio;
 
 function preload(){
-  
-  KB = loadImage("KB.jpg");
-  KBA = loadSound("Kill Bill Audio.mp3")
+  kardRM = loadImage("KARD red moon.jpg");
 }
 
 
@@ -13,48 +11,42 @@ function preload(){
 let container;
 let button1;
 let button2;
-let button3;
+let slider;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
     
-    button1 = createButton ('!st song');
-    button2 = createButton ('2nd song');
-    button3 = createButton ('3rd song');
+    button1 = createButton ('PLAY');
+    button2 = createButton ('Pause');
+    slider = createSlider (0,176,0);
 
     
     button1.class ('button-class');
     button2.class ('button-class');
-    button3.class ('button-class');
-  
-    button1.mousePressed(killBill())
-
     
     container = createDiv()
     container.child(button1);
     container.child(button2);
-    container.child(button3);
 
-    container.position(width/2-300/2, height/2);
+
+    container.position(width/2-200/2, height/1.1);
   
 }
 
 function draw() {
   background(220);
   
-}
-
-
-function killBill(){
+  startPage()
   
-  image(KB, width/4, 0)
-  KBA.play()
 }
 
-
-
-
+function startPage(){
+  imageMode(CENTER);
+  image(kardRM, width/2,height/2)
+  
+  
+}
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
