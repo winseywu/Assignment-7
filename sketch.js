a5df0a284ec4c5f9024657773a57e2a7d96f34d3
winseywu <70607960+winseywu@users.noreply.github.com>
2020-11-02 20:38:46 -0500

    decide smt new

--- sketch.js
+++ sketch.js
@@ -1,39 +1,62 @@
+let KB;
+let KBA;
+
+function preload(){
+  
+  KB = loadImage("KB.jpg");
+  KBA = loadSound("Kill Bill Audio.mp3")
+}
+
+
+
 // buttons and Container
 let container;
 let button1;
 let button2;
 let button3;
-let button4;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
     
-    button1 = createButton ('the 1st button');
-    button2 = createButton ('the 2nd button');
-    button3 = createButton ('the 3rd button');
-    button4 = createButton ('the 4th button');
+    button1 = createButton ('!st song');
+    button2 = createButton ('2nd song');
+    button3 = createButton ('3rd song');
+
     
     button1.class ('button-class');
     button2.class ('button-class');
     button3.class ('button-class');
-    button4.class ('button-class');
+  
+    button1.mousePressed(killBill())
+
     
     container = createDiv()
-    container.id = ('button-container')
-    
-    
     container.child(button1);
     container.child(button2);
     container.child(button3);
-    container.child(button4);
-    
-    container.position(width/2, height/2);
+
+    container.position(width/2-300/2, height/2);
+  
 }
 
 function draw() {
   background(220);
+  
 }
+
+
+function killBill(){
+  
+  image(KB, width/4, 0)
+  KBA.play()
+}
+
+
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  container.position(width/2-300/2, height/2);
 }
